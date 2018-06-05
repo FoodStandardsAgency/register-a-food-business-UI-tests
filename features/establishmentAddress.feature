5d4d457f27b3839beecae76bc9df016f462edbde
libertyjacklin1 <liberty.jacklin@gmail.com>
2018-06-05 11:35:37 +0100

    Happy Path scenario successful, sorting error checking for testing error validation

--- features/establishmentAddress.feature
+++ features/establishmentAddress.feature
@@ -2,9 +2,22 @@ Feature: Establishment Address
 
    Establishment address section validation
 
+   @qa_ready
    Scenario: testing happy path
     Given I am on the establishment address page
-    When I put the Establishment first line and post code in and I press "save and continue"
+    When I put a valid Establishment first line in
+    And I put a valid post code in
+    And I press save and continue
     Then I am taken to another page
+
+    @not_done
+    Scenario: 
+    Given I am on the establishment address page
+    When I put a valid Establishment first line in
+    And I put an invalid post code in
+    When I press save and continue
+    Then an error appears
+
+
 
 
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -7,15 +7,15 @@ const { Builder, By, until, Key } = require("selenium-webdriver");
 let driver = new Builder().forBrowser("chrome").build();
 require("dotenv").config();
 
+//form fields
 var establishmentFirstLine;
-
 var establishmentPostCode;
+//current url
+var urltest;
 
 Given("I am on the establishment address page", async function() {
   await driver.get(
-    `http://${process.env.USERNAME}:${process.env.PASSWORD}@${
-      process.env.URLBASE
-    }${process.env.ESTABLISHMENTADDRESSPAGE}`
+    `http://${process.env.URLBASE}${process.env.ESTABLISHMENTADDRESSPAGE}`
   );
   establishmentFirstLine = driver.findElement(
     By.id("establishment_first_line")
@@ -23,16 +23,40 @@ Given("I am on the establishment address page", async function() {
   establishmentPostCode = driver.findElement(By.id("establishment_postcode"));
 });
 
-When(
-  'I put the Establishment first line and post code in and I press "save and continue"',
+When("I put a valid Establishment first line in", async function() {
+  await establishmentFirstLine.sendKeys("Example");
+
+  //establishmentPostCode.sendKeys(Key.ENTER);
+});
+When("I put a valid post code in", async function() {
+  await establishmentPostCode.sendKeys("W11 4ET");
+});
+
+When("I put an invalid post code in", async function() {
+  await establishmentPostCode.sendKeys("notapostcode");
+});
+
+When("I press save and continue", async function() {
+  await establishmentPostCode.sendKeys(Key.ENTER);
+});
+
+Then("I am taken to another page", async function() {
+  var urltest = await driver.getCurrentUrl();
+  console.log("THIS IS THE URL", urltest);
+  assert.equal(urltest, "http://localhost:3000/submit", "URLs don't match");
+});
+
+Given(
+  "I have filled in the post code with invalid characters",
   async function() {
     await establishmentFirstLine.sendKeys("Example");
-    await establishmentPostCode.sendKeys("W11 4ET");
+    await establishmentPostCode.sendKeys("adfjanfanfa");
     establishmentPostCode.sendKeys(Key.ENTER);
   }
 );
 
-Then("I am taken to another page", function() {
-  //test that the url is equal to the submit one
-  assert.equal(this.getCurrentUrl, "http://localhost:3000/submit");
+Then("an error appears", async function() {
+  var error = await driver.getElement();
+  console.log("Error message", error);
+  assert.equal(error, "error text here", "error message doesnt match");
 });

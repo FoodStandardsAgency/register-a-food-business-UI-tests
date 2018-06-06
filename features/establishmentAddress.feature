2d2fe146e4df3a209822fd43a78aec064187a92f
libertyjacklin1 <liberty.jacklin@gmail.com>
2018-06-06 13:04:22 +0100

    Sprint 1 Tests

--- features/establishmentAddress.feature
+++ features/establishmentAddress.feature
@@ -1,22 +1,42 @@
+@establishment_address
 Feature: Establishment Address
 
    Establishment address section validation
 
-   @qa_ready
+   @happy_path
    Scenario: testing happy path
-    Given I am on the establishment address page
+    Given I have opened a new window 
+    And I am on the establishment address page
     When I put a valid Establishment first line in
     And I put a valid post code in
     And I press save and continue
     Then I am taken to another page
+    And it closes the window
 
-    @not_done
-    Scenario: 
-    Given I am on the establishment address page
+    @invalid_postcode
+    Scenario: Invalid Postcode
+    
+    Given I have opened a new window 
+    And I am on the establishment address page
     When I put a valid Establishment first line in
     And I put an invalid post code in
-    When I press save and continue
-    Then an error appears
+    And I press save and continue
+    Then an error appears telling me my postcode is invalid
+    And the valid Establishment first line is still there
+    And the invalid PostCode is still there
+    And it closes the window
+
+    @invalid_firstline
+    Scenario: Valid Postcode and not putting in first line
+    Given I have opened a new window 
+    And I am on the establishment address page
+    When I put a valid post code in
+    And I press save and continue
+    Then an error appears telling me my first line is invalid
+    And the valid PostCode is still there
+     And it closes the window
+
+
 
 
 
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -2,34 +2,59 @@ const assert = require("assert");
 const { Given, When, Then } = require("cucumber");
 var { setDefaultTimeout } = require("cucumber");
 setDefaultTimeout(60 * 1000);
-const { Builder, By, until, Key } = require("selenium-webdriver");
-//const webdriver = require("selenium-webdriver");
-let driver = new Builder().forBrowser("chrome").build();
+//const { Builder, By, until, Key } = require("selenium-webdriver");
+
+const webdriver = require("selenium-webdriver");
+var driver;
+
 require("dotenv").config();
 
 //form fields
 var establishmentFirstLine;
 var establishmentPostCode;
+
+//validdetails
+var validPostCode = "W11 4ET";
+var validFirstLine = "Example Establishment Address";
+
+//invaliddetails
+var invalidPostCode = "notapostcode";
+var nothing = "";
+
 //current url
 var urltest;
 
+//error
+var error;
+
+//////ESTABLISHMENT ADDRESS /////////
+
+Given("I have opened a new window", async function() {
+  driver = await new webdriver.Builder().forBrowser("chrome").build();
+});
+
 Given("I am on the establishment address page", async function() {
+  //newWindow = await driver.webdriver.createSession();
+  // await new webdriver.Builder().forBrowser("chrome").build();
+
   await driver.get(
     `http://${process.env.URLBASE}${process.env.ESTABLISHMENTADDRESSPAGE}`
   );
   establishmentFirstLine = driver.findElement(
-    By.id("establishment_first_line")
+    webdriver.By.name("establishment_first_line")
   );
-  establishmentPostCode = driver.findElement(By.id("establishment_postcode"));
+  establishmentPostCode = driver.findElement(
+    webdriver.By.name("establishment_postcode")
+  );
 });
 
 When("I put a valid Establishment first line in", async function() {
-  await establishmentFirstLine.sendKeys("Example");
+  await establishmentFirstLine.sendKeys(validFirstLine);
 
   //establishmentPostCode.sendKeys(Key.ENTER);
 });
 When("I put a valid post code in", async function() {
-  await establishmentPostCode.sendKeys("W11 4ET");
+  await establishmentPostCode.sendKeys(validPostCode);
 });
 
 When("I put an invalid post code in", async function() {
@@ -37,26 +62,177 @@ When("I put an invalid post code in", async function() {
 });
 
 When("I press save and continue", async function() {
-  await establishmentPostCode.sendKeys(Key.ENTER);
+  //await establishmentPostCode.sendKeys(webdriver.Key.ENTER);
+  await establishmentPostCode.submit();
 });
 
 Then("I am taken to another page", async function() {
-  var urltest = await driver.getCurrentUrl();
+  urltest = await driver.getCurrentUrl();
   console.log("THIS IS THE URL", urltest);
-  assert.equal(urltest, "http://localhost:3000/submit", "URLs don't match");
+  assert.notEqual(
+    urltest,
+    `http://${process.env.URLBASE}${process.env.ESTABLISHMENTADDRESSPAGE}`,
+    "URLs do match"
+  );
 });
 
-Given(
-  "I have filled in the post code with invalid characters",
-  async function() {
-    await establishmentFirstLine.sendKeys("Example");
-    await establishmentPostCode.sendKeys("adfjanfanfa");
-    establishmentPostCode.sendKeys(Key.ENTER);
-  }
-);
+Then("the valid Establishment first line is still there", async function() {
+  var currentFirstLine = await driver
+    .findElement(webdriver.By.name("establishment_first_line"))
+    .getAttribute("value");
+  console.log("FIRST LINE", currentFirstLine);
+  assert.equal(currentFirstLine, validFirstLine, "text is no longer there");
+});
 
-Then("an error appears", async function() {
-  var error = await driver.getElement();
-  console.log("Error message", error);
-  assert.equal(error, "error text here", "error message doesnt match");
+Then("the invalid PostCode is still there", async function() {
+  currentPostCode = await driver
+    .findElement(webdriver.By.name("establishment_postcode"))
+    .getAttribute("value");
+  console.log("CURRENT POST CODE", currentPostCode);
+  assert.equal(currentPostCode, invalidPostCode, "text is no longer there");
+});
+
+Then("an error appears telling me my postcode is invalid", async function() {
+  //the following works but using classname, which isnt ideal as is likely to change
+  postCodeError = await driver.findElement(
+    webdriver.By.className("css-jdwgdl")
+  );
+  postCodeErrorText = await postCodeError.getText();
+  console.log("Postcode Error Text", postCodeErrorText);
+  assert.equal(
+    postCodeErrorText,
+    "Not a valid Postcode",
+    "error message doesnt match"
+  );
+  //the below doesn't work
+  // postcode = await driver.findElement(
+  //   webdriver.By.js(function() {
+  //     var pcode = document.getElementsByTagName("span");
+  //   })
+  // );
+  // console.log("POSTCODE", postcode);
+});
+
+Then("an error appears telling me my first line is invalid", async function() {
+  //the following works but using classname, which isnt ideal as is likely to change
+  firstLineError = await driver.findElement(
+    webdriver.By.className("css-jdwgdl")
+  );
+  firstLineErrorText = await firstLineError.getText();
+  console.log("First Line Error Text", firstLineErrorText);
+  assert.equal(
+    firstLineErrorText,
+    "Not a valid First Line of address",
+    "error message doesnt match"
+  );
+});
+
+Then("the valid PostCode is still there", async function() {
+  currentPostCode = await driver
+    .findElement(webdriver.By.name("establishment_postcode"))
+    .getAttribute("value");
+  console.log("CURRENT POST CODE", currentPostCode);
+  assert.equal(currentPostCode, validPostCode, "text is no longer there");
+});
+
+// Given("I open a window", async function() {
+//   driver = new webdriver.Builder().forBrowser("chrome").build();
+// });
+
+Then("it closes the window", async function() {
+  quit = driver.quit();
+});
+
+///////// LANDING PAGE /////////
+
+// Given("I have opened a new window for the landing page", async function() {
+//   driver = await new webdriver.Builder().forBrowser("chrome").build();
+// });
+
+require("dotenv").config();
+
+var beginButton;
+var theUrl;
+Given("that I am on the start page", async function() {
+  await driver.get(`http://${process.env.URLBASE}`);
+});
+
+When("I click begin registration", async function() {
+  beginButton = await driver.findElement(webdriver.By.className("css-wu1qf"));
+  beginButton.submit();
+});
+
+Then("I am directed to another page", async function() {
+  theUrl = await driver.getCurrentUrl();
+  console.log("THIS IS THE URL", theUrl);
+  assert.notEqual(theUrl, "http://localhost:3000/", "URLs match");
+});
+
+////////SUBMIT REGISTRATION ////
+
+require("dotenv").config();
+var form1;
+var form2;
+var form3;
+
+//current url
+var currentUrl;
+var boxSelected;
+
+// Given(
+//   "I have opened a new window for the submit registration",
+//   async function() {
+//     driver = await new webdriver.Builder().forBrowser("chrome").build();
+//   }
+// );
+
+Given("that I am on the declaration page", async function() {
+  await driver.get(`http://${process.env.URLBASE}${process.env.DECLARATION}`);
+  form1 = driver.findElement(webdriver.By.name("declaration1"));
+  form2 = driver.findElement(webdriver.By.name("declaration2"));
+  form3 = driver.findElement(webdriver.By.name("declaration3"));
+});
+
+Given("I have ticked all the boxes", async function() {
+  form1.click();
+  form2.click();
+  form3.click();
+});
+
+Given("I have ticked one of the boxes", async function() {
+  form1.click();
+});
+
+When("I click submit", async function() {
+  await form3.submit();
+});
+
+Then("I am shown an error", async function() {
+  declarationError = await driver.findElement(
+    webdriver.By.className("css-jdwgdl")
+  );
+  declarationErrorText = await declarationError.getText();
+  console.log("First Line Error Text", declarationErrorText);
+  assert.equal(
+    declarationErrorText,
+    "You must tick all the declarations before continuing",
+    "error message doesnt match"
+  );
+});
+
+Then("I am directed to the application complete page", async function() {
+  currentUrl = await driver.getCurrentUrl();
+  console.log("THIS IS THE URL", currentUrl);
+  assert.equal(
+    currentUrl,
+    "http://localhost:3000/application-complete",
+    "URLs don't match"
+  );
+});
+
+Then("my box is still ticked", async function() {
+  form1 = driver.findElement(webdriver.By.name("declaration1"));
+  boxSelected = await form1.isSelected();
+  console.log("box selected", boxSelected);
+  assert.equal(boxSelected, true, "box isn't selected");
 });

5a3fe47738b497aed0ffd1b3662872b0dc0da9a1
libertyjacklin1 <liberty.jacklin@gmail.com>
2018-09-03 13:34:45 +0100

    Updating page object file

--- src/pageObjects/commonElements.page.js
+++ src/pageObjects/commonElements.page.js
@@ -3,12 +3,11 @@ const commonElements = {
     betaBanner: ".css-1vpsoh9",
     betaBeta: ".css-15nm2se",
     betaLink: "#feedbackLink",
-    cookieBanner: ".css-17s6ti4.e1n0mw0n3",
+    cookieBanner: "#cookieBanner",
     cookieInfo: "#cookieInfo",
     cookiePolicy: "#cookiePolicy",
     cookieAccept: "#cookieAccept",
     cookieReject: "#cookieReject",
-    cookieRejectText: "#cookieRejectText",
     cookieClose: "#cookieClose"
 };
 

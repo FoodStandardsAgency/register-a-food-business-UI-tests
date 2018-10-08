40de1c5bc132f2c8d1111a95b2042ae5579e64e9
A. P. Malde <anisha.malde@ibm.com>
2018-10-08 17:10:13 +0100

    Debugging changes

--- src/pageObjects/openingDaysSome.page.js
+++ src/pageObjects/openingDaysSome.page.js
@@ -1,13 +1,13 @@
 const openingDaysSome = {
-    monday: "#opening_day_monday",
-    tuesdayt: "#opening_day_tuesday",
-    wednesday: "#opening_day_wednesday",
-    thursday: "#opening_day_thursday",
-    friday: "#opening_day_friday",
-    saturday: "#opening_day_saturday",
-    sunday: "#opening_day_sunday",
-    button: "#continue-button",
-    error: ".css-jdwgdl"
+  monday: "#opening_day_monday",
+  tuesday: "#opening_day_tuesday",
+  wednesday: "#opening_day_wednesday",
+  thursday: "#opening_day_thursday",
+  friday: "#opening_day_friday",
+  saturday: "#opening_day_saturday",
+  sunday: "#opening_day_sunday",
+  button: "#continue-button",
+  error: ".css-jdwgdl"
 };
 
 module.exports = openingDaysSome;
--- wdio.conf.js
+++ wdio.conf.js
@@ -66,7 +66,8 @@ exports.config = {
       "./src/features/**/establishmentOpeningDate.feature",
       "./src/features/**/establishmentTradingName.feature",
       "./src/features/**/establishmentAddress.feature",
-      "./src/features/**/establishmentAddressType.feature"
+      "./src/features/**/establishmentAddressType.feature",
+      "./src/features/**/establishmentOpeningDays.feature"
     ],
     operator: [
       "./src/features/**/operatorAddress.feature",

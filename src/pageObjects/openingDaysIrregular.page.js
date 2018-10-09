8ed2ade49a220c75d78d2396efea26142166770f
A. P. Malde <anisha.malde@ibm.com>
2018-10-09 12:13:41 +0100

    Typo change

--- src/pageObjects/openingDaysIrregular.page.js
+++ src/pageObjects/openingDaysIrregular.page.js
@@ -1,7 +1,7 @@
 const openingDaysIrregular = {
-    otherDaysIrregular: '[name="opening_days_irregular"]',
-    button: "#continue-button",
-    error: ".css-jdwgdl"
+  otherDaysIrregular: "#opening_days_irregular",
+  button: "#continue-button",
+  error: ".css-jdwgdl"
 };
 
 module.exports = openingDaysIrregular;

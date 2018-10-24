b47d10cd0117fa0c90967ad2898bb942f6c10148
jennyoughibm <43647399+jennyoughibm@users.noreply.github.com>
2018-10-24 13:11:39 +0100

    Update run_local_outline.sh

--- run_local_outline.sh
+++ run_local_outline.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/env bash
 echo "Running end2end test suite locally"
 ./node_modules/.bin/wdio --suite secnariooutline --baseUrl https://dev-register-a-food-business.azurewebsites.net/new/ wdio.conf.js

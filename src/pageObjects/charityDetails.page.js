44bdd692ad7d401482d01161f54e5a51678d7d16
Simon Thomas <simon.thomas@civica.co.uk>
2020-05-12 14:49:24 +0100

    update chatiry question selector

--- src/pageObjects/charityDetails.page.js
+++ src/pageObjects/charityDetails.page.js
@@ -1,7 +1,7 @@
 const charityDetails = {
   charityName: '[name="operator_charity_name"]',
   charityNumber: '[name="operator_charity_number"]',
-  questionsCharityReference: "#hiddenTextCharityNumbers",
+  questionsCharityReference: "form details span",
   link: "#link-charity-commission",
   button: "#continue-button",
   error: "#errorSummary"

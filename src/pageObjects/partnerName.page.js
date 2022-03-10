bab607904b70d57fa39081b7d9576c70566dd0db
Simon Thomas <simon.thomas@civica.co.uk>
2022-03-10 14:02:25 +0000

    identify continue button by class

--- src/pageObjects/partnerName.page.js
+++ src/pageObjects/partnerName.page.js
@@ -1,15 +1,14 @@
-const partnerName = { 
-    addPartnerButton: "#addPartnerButton",
-    partnerOne: "#partner0",
-    partnerTwo: "#partner1",
-    partnerOneRowChange: "#partner0RowChange",
-    deleteButton0: "#deletePartnerButton0",
-    deleteButton1: "#deletePartnerButton1",
-    deleteButton2: "#deletePartnerButton2",
-    deleteButton3: "#deletePartnerButton3",
-    button: ".govuk-button",
-    error: ".govuk-error-message"
-  };
-  
-  module.exports = partnerName;
-  
+const partnerName = {
+  addPartnerButton: "#addPartnerButton",
+  partnerOne: "#partner0",
+  partnerTwo: "#partner1",
+  partnerOneRowChange: "#partner0RowChange",
+  deleteButton0: "#deletePartnerButton0",
+  deleteButton1: "#deletePartnerButton1",
+  deleteButton2: "#deletePartnerButton2",
+  deleteButton3: "#deletePartnerButton3",
+  button: ".continue",
+  error: ".govuk-error-message",
+};
+
+module.exports = partnerName;

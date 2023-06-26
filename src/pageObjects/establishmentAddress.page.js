eea4906a1873b1ef9e313fdbad6f5c6794cefea1
kieran clifton <kieran.clifton@civica.co.uk>
2023-06-26 14:17:21 +0100

    changed selector

--- src/pageObjects/establishmentAddress.page.js
+++ src/pageObjects/establishmentAddress.page.js
@@ -1,28 +1,34 @@
-const estabAddress = { 
+const estabAddress = {
   firstline: '[name="establishment_address_line_1"]',
   postcode: '[name="establishment_postcode_find"]',
   manualPostcode: '[name="establishment_postcode"]',
   findAddress: "#find-address",
   firstOption: "#local_authority__option--0",
   postcodeDisplay: "#main-content > div > div > form > fieldset > legend > h1",
-  cannotFindPostcodeHeading: "#main-content > div > div > fieldset > legend > h1",
-  postcodeLocator:"#local_authority",
+  cannotFindPostcodeHeading:
+    "#main-content > div > div > fieldset > legend > h1",
+  postcodeLocator: "#local_authority",
   estabPostcodeFind: "#establishment_postcode_find",
-  noCouncilHeading:"#main-heading",
-  changePostcode: "#main-content > div > div > form > dl > div > dd.govuk-summary-list__actions > a",
+  noCouncilHeading: "#main-heading",
+  changePostcode:
+    "#main-content > div > div > form > dl > div > dd.govuk-summary-list__actions > a",
   postcodeDropdown: "#establishmentAddressDropdown",
-  street: '[name="establishment_address_line_2"]', 
+  street: '[name="establishment_address_line_2"]',
   town: '[name="establishment_town"]',
   button: ".govuk-button",
   error: ".govuk-error-message",
   cantFindAddressLink: "#cantFindAddressLink",
   locality: '[name="establishment_address_line_3"]',
-  backButton: "body > div.govuk-width-container > nav > div.govuk-grid-row > div.govuk-grid-column-two-thirds > a",
-  backButtonEstablished:"body > div > nav > div.govuk-grid-row > div.govuk-grid-column-two-thirds > a",
-  cantFindLA:"#main-content > div > div > form > div:nth-child(2) > div > div > label",
-  postcodeFinder:"#postcode",
-  errorPostcode:"#error-summary-ba73298e > div > h2",
+  backButton:
+    "body > div.govuk-width-container > nav > div.govuk-grid-row > div.govuk-grid-column-two-thirds > a",
+  backButtonEstablished:
+    "body > div > nav > div.govuk-grid-row > div.govuk-grid-column-two-thirds > a",
+  cantFindLA:
+    "#main-content > div > div > form > div:nth-child(2) > div > div > label",
+  postcodeFinder: "#postcode",
+  errorPostcode: "#error-summary-ba73298e > div > h2",
   LAnotOnboarded: "#content > h1.heading-large",
-  LAInvalidError: "#error-summary-title"
+  LAInvalidError:
+    "#main-content > div > div > form > div.govuk-error-summary > div > h2",
 };
 module.exports = estabAddress;

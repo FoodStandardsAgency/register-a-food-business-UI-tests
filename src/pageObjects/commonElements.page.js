7e15aa46988dbc4a896612d664fb6383969254db
kieran clifton <kieran.clifton@civica.co.uk>
2023-06-27 09:55:08 +0100

    test to see if test pass

--- src/pageObjects/commonElements.page.js
+++ src/pageObjects/commonElements.page.js
@@ -2,7 +2,7 @@ const commonElements = {
   backButton:
     "body > div > nav > div.govuk-grid-row > div.govuk-grid-column-two-thirds > a",
   betaLink: "body > div > nav > div.govuk-phase-banner > p > span > a",
-  cookieBanner: "body > div.govuk-cookie-banner",
+  cookieBanner: ".govuk-cookie-banner__message",
   cookieInfo:
     "body > div.govuk-cookie-banner > div > div.govuk-button-group > a:nth-child(3)",
   cookiePolicy:

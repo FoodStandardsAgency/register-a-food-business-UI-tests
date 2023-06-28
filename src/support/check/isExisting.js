ca55698f8499a65ec58b5907fc154807a74e78ee
Simon Thomas <simon.thomas@civica.co.uk>
2023-06-28 16:17:09 +0100

    potential isExisting fix to account for false case

--- src/support/check/isExisting.js
+++ src/support/check/isExisting.js
@@ -8,6 +8,17 @@ export default (selector, falseCase) => {
      * Elements found in the DOM
      * @type {Object}
      */
-    const elements = $(selector);
-    elements.waitForExist({reverse:falseCase});
+    const elements = browser.elements(selector).value;
+
+    if (falseCase) {
+      expect(elements).to.have.lengthOf(
+        0,
+        `Expected element "${selector}" not to exist`
+      );
+    } else {
+      expect(elements).to.have.length.above(
+        0,
+        `Expected element "${selector}" to exist`
+      );
+    }
 };

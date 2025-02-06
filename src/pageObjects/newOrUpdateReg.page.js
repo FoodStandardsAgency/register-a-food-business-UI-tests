69d60f863405cb6661abc9976763ee19cd7e377d
Joe-Cun-Civica <joseph.cunningham@civica.co.uk>
2025-02-06 14:41:31 +0000

    adding phone number and email

--- src/pageObjects/newOrUpdateReg.page.js
+++ src/pageObjects/newOrUpdateReg.page.js
@@ -3,5 +3,7 @@ const newOrUpdateReg = {
   heading: "#main-content > div > div > form > div > fieldset > legend > h1",
   newRegistration: "#new_registration",
   updateReg: "#update_registration",
+  phoneNumber: "#main-content > div > div > p:nth-child(2) > a",
+  emailAddress: "#main-content > div > div > p:nth-child(2) > a",
 };
 export default newOrUpdateReg;
